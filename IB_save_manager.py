ca8125b7d981498ac10df41e46603a23eaa29cfd
ben-my-to <my.toe.ben@gmail.com>
2024-07-03 01:44:56 -0700

    updated to replace ib2 save files

--- IB_save_manager.py
+++ IB_save_manager.py
@@ -5,8 +5,8 @@ from device_manager import DeviceManager
 
 
 REMOTE_APP_DIR = Path("/private/var/mobile/Containers/Data/Application")
-LOCAL_SAVE_DIR = Path("STORE/")
 
+LOCAL_SAVE_DIR = Path("STORE/")
 LOCAL_SAVE_DIR.mkdir(exist_ok=True)
 
 IB_SAVE_FILES = (
@@ -25,6 +25,13 @@ if len(sys.argv) != 2:
     print("python IB_save_manager.py <hostname>")
     sys.exit(1)
 
+
+def collapse_file_path(path: Path):
+    root_path = path.parents[-2]
+    filename = path.name
+    return f"{root_path}/.../{filename}"
+
+
 dvm = DeviceManager(hostname=sys.argv[1])
 device = dvm.get_device()
 
@@ -32,13 +39,34 @@ _, stdout, _ = device.exec_command(f"find {REMOTE_APP_DIR} -name SwordSave.bin")
 save_rel_path = stdout.read().decode()
 
 REMOTE_SAVE_DIR = (REMOTE_APP_DIR / save_rel_path).parents[0]
+logger.info(f"Located IB Save Directory at {REMOTE_SAVE_DIR}")
+
+logger.info("Replacing IB2 Save Files")
+
+REMOTE_IB2_SAVE_FILES = (
+    REMOTE_SAVE_DIR / IB_SAVE_FILES[1],
+    REMOTE_SAVE_DIR / IB_SAVE_FILES[2],
+)
+
+REMOTE_IB2_BACKUP_SAVES = (
+    REMOTE_SAVE_DIR / IB_SAVE_FILES[3],
+    REMOTE_SAVE_DIR / IB_SAVE_FILES[4],
+)
+
+for backup, save in zip(REMOTE_IB2_BACKUP_SAVES, REMOTE_IB2_SAVE_FILES):
+    device.exec_command(f"cp -f {backup} {save}")
+    logger.info(f"Copied {collapse_file_path(backup)} to {collapse_file_path(save)}")
+
+logger.info("Backing Up Save Files")
 
 sftp = device.open_sftp()
 for filename in IB_SAVE_FILES:
-    local_save_file = LOCAL_SAVE_DIR / Path(filename).name
-    remote_save_file = (REMOTE_SAVE_DIR / filename).as_posix()
-    sftp.get(remote_save_file, local_save_file)
-    logger.info(f"Saved {remote_save_file} to {local_save_file}")
+    local_save_file = (
+        LOCAL_SAVE_DIR / Path(filename).name
+    )  # NOTE: store only the files, not any directories
+    remote_save_file = REMOTE_SAVE_DIR / filename
+    sftp.get(remote_save_file.as_posix(), local_save_file)
+    logger.info(f"Copied {collapse_file_path(remote_save_file)} to {local_save_file}")
 sftp.close()
 
 dvm.release()
